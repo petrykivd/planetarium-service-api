fbb0f4db226c6ff88d1304976634cb1fdcfbaf68
petrykivd <petrykiv.dmytro19@gmail.com>
2023-12-11 16:38:51 +0200

    Added urls for tokens and documentation

--- planetarium/urls.py
+++ planetarium/urls.py
@@ -2,12 +2,15 @@ from django.urls import path, include
 from rest_framework import routers
 
 from planetarium.views import (
-    ShowThemeViewSet, AstronomyShowViewSet
+    ShowThemeViewSet, AstronomyShowViewSet, ShowSessionViewSet, ReservationViewSet
 )
 
 router = routers.DefaultRouter()
 router.register("show_themes", ShowThemeViewSet)
+router.register("planetarium_domes", AstronomyShowViewSet)
 router.register("astronomy_shows", AstronomyShowViewSet)
+router.register("show_sessions", ShowSessionViewSet)
+router.register("reservations", ReservationViewSet)
 
 
 urlpatterns = [path("", include(router.urls))]
